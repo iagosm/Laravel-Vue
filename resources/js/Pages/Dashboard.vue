432157c4cf4ced4bfeb65942babbfaa152b10f6c
iagosm <iagosousa201486@gmail.com>
2025-02-19 18:41:31 -0300

    Consumindo api com axios, utilizando onMounted e ref

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,23 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+    import { ref, onMounted, watch } from 'vue';
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import { Head } from '@inertiajs/vue3';
+    import axios from 'axios';
+
+
+    const usuarios = ref([]);
+
+    // ref => referencia, sempre que alterar ele irá atualizar a variavel
+    // onmonted => assim que iniciar / abrir a pagina ira chamar a função
+    const buscarUsuarios = async () => {
+        const resposta = await axios.get('/api/usuarios');
+        usuarios.value = resposta.data
+        console.log('resposta', usuarios)
+    }
+
+    onMounted(() => {
+        buscarUsuarios();
+    })
 </script>
 
 <template>
@@ -17,11 +34,13 @@ import { Head } from '@inertiajs/vue3';
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
-                >
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg" >
                     <div class="p-6 text-gray-900">
-                     Bem Vindo!
+                        Bem Vindo!
+                        <div v-for="usuario in usuarios">
+                            <p>{{ usuario.nome }} | {{ usuario.email }} | {{ usuario.telefone }}</p>
+                            <br><hr>
+                        </div>
                     </div>
                 </div>
             </div>
